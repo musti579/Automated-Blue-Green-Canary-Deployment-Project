51c2c9449ce4e2f96047239db0df9dbaf245ee38
Mustafa Mohamoud <mustafaabukar13@gmail.com>
2026-07-25 17:09:33 +0100

    debug in root main public subnet variable not effectively mentioned

--- terraform/modules/endpoint/main.tf
+++ terraform/modules/endpoint/main.tf
@@ -10,67 +10,63 @@ resource "aws_vpc_endpoint" "s3_endpoint" {
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id            =  var.vpc_id
-  service_name      = "com.amazonaws.eu-north-1.ecr.api"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.eu-north-1.ecr.api"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
-  
+  subnet_ids          = var.private_subnet_ids
 
   security_group_ids = [
     aws_security_group.endpoint_sg.id,
   ]
-
- 
 }
 
 resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.eu-north-1.ecr.dkr"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.eu-north-1.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
+  subnet_ids          = var.private_subnet_ids
 
   security_group_ids = [
     aws_security_group.endpoint_sg.id,
   ]
-
 }
 
 resource "aws_vpc_endpoint" "sqs_endpoint" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.eu-north-1.sqs"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.eu-north-1.sqs"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
+  subnet_ids          = var.private_subnet_ids
 
   security_group_ids = [
     aws_security_group.endpoint_sg.id,
   ]
-
-  
 }
 
 resource "aws_vpc_endpoint" "cloudwatch_endpoint" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.eu-north-1.logs"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.eu-north-1.logs"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
+  subnet_ids          = var.private_subnet_ids
 
   security_group_ids = [
     aws_security_group.endpoint_sg.id,
   ]
-
-  
 }
 
 resource "aws_vpc_endpoint" "secret_manager_endpoint" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.eu-north-1.secretsmanager"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.eu-north-1.secretsmanager"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
+  subnet_ids          = var.private_subnet_ids
 
   security_group_ids = [
     aws_security_group.endpoint_sg.id,
   ]
-
 }
 
 resource "aws_security_group" "endpoint_sg" {
@@ -78,17 +74,15 @@ resource "aws_security_group" "endpoint_sg" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp" 
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = [var.vpc_cidr]
-
   }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
