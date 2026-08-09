d6d7f97eaa3b37a3e8dff00d0c5de236cf033499
Mustafa Mohamoud <mustafaabukar13@gmail.com>
2026-08-09 16:10:14 +0100

    Creates the DNS record ACM needs to verify we own the domain

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -8,3 +8,24 @@ resource "aws_acm_certificate" "api_cert" {
   }
 }
 
+# Looks up the existing Route 53 hosted zone for urlshortening.net
+data "aws_route53_zone" "main" {
+  name = "urlshortening.net"
+}
+
+# Creates the DNS record ACM needs to verify we own the domain
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
